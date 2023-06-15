9a3ef5cba085471f643b94cc12cae8f8d5e98f82
shikhasethi1203 <110726410+shikhasethi1203@users.noreply.github.com>
2023-06-15 18:48:12 +0530

    add the arary of object

---
+++ src/compontents/Data.js
@@ -0,0 +1,53 @@
+
+const array=[
+    {
+      "id": 1,
+      "phone": "Phone 1",
+      "image": "public/gettyimages-1468873400-612x612.jpg"
+    },
+    {
+      "id": 2,
+      "phone": "Phone 2",
+      "image": "public/images (1).jpg"
+    },
+    {
+      "id": 3,
+      "phone": "Phone 3",
+      "image": "public/images (2).jpg"
+    },
+    {
+      "id": 4,
+      "phone": "Phone 4",
+      "image": "public/images (3).jpg"
+    },
+    {
+      "id": 5,
+      "phone": "Phone 5",
+      "image": "public/images.jpg"
+    },
+    {
+      "id": 6,
+      "phone": "Phone 6",
+      "image": "public/pexels-essow-k-1042143.jpg"
+    },
+    {
+      "id": 7,
+      "phone": "Phone 7",
+      "image": "https://example.com/images/phone7.jpg"
+    },
+    {
+      "id": 8,
+      "phone": "Phone 8",
+      "image": "public/photo-1546054454-aa26e2b734c7.jpg"
+    },
+    {
+      "id": 9,
+      "phone": "Phone 9",
+      "image": "public/photo-1587831991059-40958cea9ca5.jpg"
+    },
+    {
+      "id": 10,
+      "phone": "Phone 10",
+      "image": "photo-1658817774209-28024d01be2c.jpg"
+    }
+  ]
